78e7efc6280bc7d0f14ab59ecf2e9d549c7c999f
Chiranjib01 <chiranjibnath21313@gmail.com>
2022-02-03 15:14:08 +0530

    Vercel url

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,5 +1,2 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME;
-export const BASE_URL =
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:3000'
-    : process.env.VERCEL_URL;
+export const BASE_URL = process.env.NEXT_PUBLIC_VERCEL_URL;
